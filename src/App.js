fe7bd1c4434a5a9cc8db3847e9e76d2ef3e38a88
Gustavo Analise Ferreira <63479834+gustavoanalise@users.noreply.github.com>
2022-05-11 14:53:46 -0300

    Incluindo aula sobre carregamento dinâmico

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import {useState, useEffect} from "react";
 function App() {
   const [products, setProducts] = useState([]);
   const url = "http://localhost:3000/products";
- 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("")
+
   useEffect(() => {
     async function fecthData() {
       const res = await fetch(url);
@@ -15,7 +17,24 @@ function App() {
     fecthData();
   }, []);
  
-  console.log(products);
+  // 2 - add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault() // usado para não submeter formulário do jeito normal
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+
+  }
  
   return (
     <div className="App">
@@ -27,6 +46,19 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome:</span>
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            <span>Preço:</span>
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
